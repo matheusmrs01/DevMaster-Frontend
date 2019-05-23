92a3a3582874ecd63c521a0eea0e5b2e38ac3ca9
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-05-23 20:33:01 -0300

    feat: configuração do service

--- src/app/devmaster.service.ts
+++ src/app/devmaster.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter, Output} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DevmasterService {
 
-  constructor() { }
+  API_URL = 'http://localhost:8000';
+  headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  public user = null;
+
+  constructor(private http: HttpClient) { }
 }
+
