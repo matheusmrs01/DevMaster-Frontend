743da4c1e3e3c210f3c837adac269bc3ab670116
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-05-23 22:57:05 -0300

    feat: configuração do header para usuario logado

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { Location } from '@angular/common';
+
+import { DevmasterService } from '../devmaster.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  usuario = null;
+
+  constructor(private devMasterService: DevmasterService, private router: Router, private location: Location, ) { }
 
   ngOnInit() {
+    this.atualizar();
+  }
+
+  logout() {
+    this.devMasterService.logout();
+    this.atualizar();
+    this.router.navigate(['']);
+  }
+
+  atualizar(){
+    let userL = 'Usuario Logado';
+    this.usuario = JSON.parse(localStorage.getItem('Usuario Logado'));
+    console.log(this.usuario)
   }
 
 }
