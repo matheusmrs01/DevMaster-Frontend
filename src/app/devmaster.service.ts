d2733f14ebfce465c83cf98054d64a184976c4bc
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-05-23 21:24:36 -0300

    feat: criação do service que faz conexão com o DevMaster-backend

--- src/app/devmaster.service.ts
+++ src/app/devmaster.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, EventEmitter, Output} from '@angular/core';
+import { Injectable, EventEmitter, Output } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs'
 
@@ -7,10 +7,47 @@ import { Observable } from 'rxjs'
 })
 export class DevmasterService {
 
-  API_URL = 'http://localhost:8000';
+  API_URL = 'http://127.0.0.1:8000';
   headers = new HttpHeaders({ 'Content-Type': 'application/json' });
   public user = null;
 
   constructor(private http: HttpClient) { }
+
+  login(username: string, senha: string): Observable<any> {
+    const qs = { 'username': username, 'password': senha };
+    return this.http.post<any>(this.API_URL + '/login/', qs);
+  }
+
+  getUser(token): Observable<any> {
+    return this.http.get<any>(this.API_URL + '/user/', {
+      headers: new HttpHeaders().set('authorization', 'Token ' + token)
+    });
+  }
+
+  getJogador(id, token): Observable<any> {
+    return this.http.get<any>(this.API_URL + '/jogador/' + id, {
+      headers: new HttpHeaders().set('authorization', 'Token ' + token)
+    });
+  }
+
+  set(user, tokenn, tokengitlab) {
+    let userL = 'Usuario Logado';
+    let myObj = { id: user.id, username: user.username, token: tokenn, gitlab_token: tokengitlab };
+    localStorage.setItem(userL, JSON.stringify(myObj));
+
+  }
+
+  get() {
+    return this.user;
+  }
+
+  clear() {
+    let userL = 'Usuario Logado';
+    localStorage.removeItem(userL);
+  }
+
+  logout() {
+    this.clear();
+  }
 }
 
