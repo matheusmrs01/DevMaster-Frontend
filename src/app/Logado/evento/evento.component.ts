4f07a2908f0c42626b3b3b1d0612a874407c8280
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-05-26 01:38:29 -0300

    feat: mostrar os premiados de um evento

--- src/app/Logado/evento/evento.component.ts
+++ src/app/Logado/evento/evento.component.ts
@@ -9,23 +9,48 @@ import { DevmasterService } from '../../devmaster.service';
 export class EventoComponent implements OnInit {
 
   eventos = null;
+  evento = null;
+  premiacao = null;
 
   constructor(
     private devMasterService: DevmasterService
   ) { }
 
   ngOnInit() {
-    
+
     this.devMasterService.getEventos(JSON.parse(localStorage.getItem('Usuario Logado')).token).subscribe(
       Eventos => {
         this.eventos = Eventos['list'];
-        console.log(this.eventos)
       },
       Erros => {
         this.eventos = 'Error no getEventos';
       }
     );
 
+  }
+
+  getEvento(id) {
+    this.devMasterService.getEvento(JSON.parse(localStorage.getItem('Usuario Logado')).token, id).subscribe(
+      Evento => {
+        this.evento = Evento['Evento'];
+        console.log(this.evento)
+      },
+      Erros => {
+        this.evento = 'Error no getEventos';
+      }
+    );
+  }
+
+  getPremiacao(id){
+    this.devMasterService.getPremiacao(JSON.parse(localStorage.getItem('Usuario Logado')).token, id).subscribe(
+      Premiacao => {
+        this.premiacao = Premiacao['Premiacao'];
+        console.log(this.premiacao)
+      },
+      Erros => {
+        this.premiacao = 'Error no getEventos';
+      }
+    );
   }
 
 }
