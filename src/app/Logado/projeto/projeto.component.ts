032e91a7dfd434e87ec154b8bc97431067f6c9d0
Matheus Morais <matheus.silva.morais01@gmail.com>
2019-05-26 04:31:06 -0300

    fix: adptando o projeto para receber grupo

--- src/app/Logado/projeto/projeto.component.ts
+++ src/app/Logado/projeto/projeto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { GitlabService } from '../../gitlab.service';
+import { DevmasterService } from '../../devmaster.service';
 
 @Component({
   selector: 'app-projeto',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjetoComponent implements OnInit {
 
-  constructor() { }
+  projetos: any = null;
+  tipo: any = null;
+  grupos: any = null;
+
+  constructor(
+    private gitlabService: GitlabService, 
+    private routeAc: ActivatedRoute, 
+    private devMasterService: DevmasterService
+    ) { }
 
   ngOnInit() {
+    this.tipo = parseInt(this.routeAc.snapshot.paramMap.get('id'));
+    if (this.tipo == 1) {
+      this.gitlabService.getProjetos(JSON.parse(localStorage.getItem('Usuario Logado')).gitlab_token).subscribe(Projetos => {
+        this.projetos = Projetos;
+      });
+    }
+
+    if (this.tipo == 2) {
+      this.devMasterService.getGrupos().subscribe(
+        Grupos => {
+          this.grupos = Grupos['List']
+        },
+        Error => {
+          this.grupos = 'Erro no getGrupos'
+        }
+      )
+    }
   }
 
 }
